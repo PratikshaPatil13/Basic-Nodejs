e2f8b4908d508eff212f0a1c48d46f10d10aab29
PratikshaPatil13 <83706107+PratikshaPatil13@users.noreply.github.com>
2021-05-29 18:32:16 +0530

    Add files via upload

--- Task5.js
+++ Task5.js
@@ -1,3 +1,5 @@
+import router from './Task6.js';
+
 const square = require('./app.js');
 
 const calculate = (a) =>{
@@ -9,4 +11,54 @@ const calculate = (a) =>{
 console.log(__filename)
 console.log(__dirname)
 
-calculate(5);
+calculate(5)
+
+
+router.post("/page", (req, res, next) => {
+    var skip = req.body.page
+    const limit = 50
+    skip = (skip - 1)*limit
+    var fromDate = new Date(req.body.fromDate)
+    var toDate = new Date(req.body.toDate)
+    var url = req.body.url
+    var currency = req.body.currency
+    var queryObject = {type:{$ne:"debit" }}
+
+    if(fromDate) {
+        queryObject.createdAt = {
+            $gte: fromDate,
+            $lte: toDate
+        }
+    }
+    if(url){
+        queryObject = url
+    }
+    if(currency){
+        queryObject = currency
+    
+    }
+    async.parallel({
+        data: function(callback){
+            Transaction.find(queryObject,{
+                "game.id":1,
+                "transaction.id":1,
+                url:1,
+                currency:1,
+                userId:1,
+                winLoseAmt:1
+            }).exec(callback)
+        },
+        total: function(callback){
+            Transaction.aggregate([{
+                $match: queryObject
+            },{
+                $group:{
+                _id:null,
+                totalAmt:{$sum:"$winLoseAmt"}
+
+            }}]).exec(callback)
+        }
+    },res.callback)
+})
+    export default router
+
